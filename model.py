9cd8b5f3bf5ac4efc46dbe97128170e489ba6896
asamerry <asamerry72@gmail.com>
2025-05-22 17:40:34 -0700

    Began model implementation

--- model.py
+++ model.py
@@ -2,32 +2,37 @@
 # (ii) discussion of theory supporting use of the chosen optimization methods and their validation
 # (iii) the optimization results you obtained in your study
 
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-from scipy.optimize import linprog
+# Data was collected from the top 20 weighted companies on the S&P 500 from nasdaq.com. Data is organized weekly and spans the previous 5 years starting from 05.19.25. Had to swap BRK.B (#9) for HD (#21) since Berkshire stock data was unavailable. 
 
-def import_data() -> [[float]]:
-    universe = []
-    for file in directory:
-        with open(file, 'r') as data:
-            universe.append(data)
+import os
+import glob
+import pandas as pd
+import numpy as np
+
+def import_data() -> dict[str, list[float]]:
+    universe = {}
+    for path in glob.glob("data/*.csv"):
+        df = pd.read_csv(path, index_col="Date")
+        universe[os.path.basename(path).split(".csv")[0]] = df["Close"].astype(float).tolist()
     return universe
 
 def f(x, Sigma) -> float:
     return x.T @ Sigma @ x 
 
 def g1(x, mu, R) -> float:
-    return mu.T @ x - R
+    return mu.T @ x - R # R = 1.00 to 1.10 which represents a 0% to 10% ROI
 
 def g2(x) -> float:
     return sum(x) - 1
 
-def main(universe) -> None:
-    x = np.array([]) # Decision vector of portfolio weights
-    mu = np.([]) # Vector of expected return for each asset
+def L(f, g1, g2):
+    return f - lambda1*g1 - lambda2*g2
+
+def model(universe) -> None:
+    x = np.array([0.05] * 20) # Decision vector of portfolio weights
+    mu = np.array([]) # Vector of expected return for each asset
     Sigma = np.array([]) # Covariance matrix for asset returns
 
 if __name__ == '__main__':
     universe = import_data()
-    main(universe)
+    print(universe)
